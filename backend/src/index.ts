44f78ec2509d087c6c9f62a0c920e55217ea9377
saivarunpuri <saivarun.puri@gmail.com>
2026-05-20 21:41:18 +0530

    fix:bacend production build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -62,7 +62,11 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
